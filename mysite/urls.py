0f5f45c9b15f8438b72641ea0c20425234b2146e
vaseles <vaselesstolarov813@gmail.com>
2023-06-25 21:18:52 +0600

    kill modeltranslation

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns(
     path('', include('base.urls')),
