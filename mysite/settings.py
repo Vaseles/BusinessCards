7942d10677728a4909222d755bd18712a2c875fb
vaseles <vaselesstolarov813@gmail.com>
2023-06-25 21:14:02 +0600

    kill modeltranslation

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
