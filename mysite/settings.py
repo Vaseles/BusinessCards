89e3b9f07aea8728d10e2bbfb76e41a1faeb39f7
vaseles <vaselesstolarov813@gmail.com>
2023-06-25 22:45:44 +0600

    add media setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,6 +156,10 @@ STATIC_URL = 'static/'
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = BASE_DIR / 'static/media'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
